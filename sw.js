4bdd1226ee61a6e98ed814094d74cda5f19e900c
drdiegopadua <drdiegopadua@gmail.com>
2026-06-16 23:11:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,46 @@
-const CACHE_NAME = 'afohand-v1';
-const urlsToCache = [
+const CACHE_NAME = 'afohand-v2';
+const STATIC_ASSETS = [
   '/',
   '/index.html',
   '/manifest.json'
 ];
 
+// Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
+// Fetch
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        if (response) return response;
+        return fetch(event.request)
+          .then((fetchResponse) => {
+            return fetchResponse;
+          });
+      })
+      .catch(() => {
+        // Offline fallback
+        return new Response('Offline');
       })
   );
 });
 
+// Activate
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
